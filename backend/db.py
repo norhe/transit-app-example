c7b55bf77df4c995063bbc200a1bf46b0681d813
Ehron Gwinn <ehron@local>
2018-12-21 10:28:36 -0500

    app.py

--- backend/db.py
+++ backend/db.py
@@ -1,8 +1,6 @@
 import mysql.connector
 from mysql.connector import errorcode
 
-TABLES = {}
-
 customer_table = '''
 CREATE TABLE IF NOT EXISTS `customers` (
     `cust_no` int(11) NOT NULL AUTO_INCREMENT,
@@ -16,24 +14,38 @@ CREATE TABLE IF NOT EXISTS `customers` (
     PRIMARY KEY (`cust_no`)
 ) ENGINE=InnoDB;'''
 
-def init_db(uri, prt, uname, pw, db):
-    conn = connect_db(uri, prt, uname, pw)
-    cursor = conn.cursor()
-    print("Preparing database {}...".format(db))
-    cursor.execute('CREATE DATABASE IF NOT EXISTS `{}`'.format(db))
-    cursor.execute('USE `{}`'.format(db))
-    print("Preparing customer table...")
-    cursor.execute(customer_table)
+class DbClient:
+    conn = None
 
-def connect_db(uri, prt, uname, pw):
-    print('Connecting to {} with username {} and password {}'.format(uri, uname, pw))
-    try:
-        conn = mysql.connector.connect(user=uname, password=pw, host=uri, port=prt)
-        return conn
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-        else:
-            print(err)
+    def __init__(self, uri, prt, uname, pw, db):
+        self.init_db(uri, prt, uname, pw, db)
+
+    def init_db(self, uri, prt, uname, pw, db):
+        self.connect_db(uri, prt, uname, pw)
+        cursor = self.conn.cursor()
+        print("Preparing database {}...".format(db))
+        cursor.execute('CREATE DATABASE IF NOT EXISTS `{}`'.format(db))
+        cursor.execute('USE `{}`'.format(db))
+        print("Preparing customer table...")
+        cursor.execute(customer_table)
+        cursor.close()
+
+    def connect_db(self, uri, prt, uname, pw):
+        print('Connecting to {} with username {} and password {}'.format(uri, uname, pw))
+        try:
+            self.conn = mysql.connector.connect(user=uname, password=pw, host=uri, port=prt)
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+
+    def get_customer_records(self, num):
+        cursor = self.conn.cursor()
+
+    # Records should be a dictionary of one or more customer records like:
+    # 
+    def insert_customer_records(self, records):
+        cursor = self.conn.cursor()
