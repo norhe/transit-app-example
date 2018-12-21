d8d92230aa2979f6417c1155b0a3521bc4b978a7
Ehron Gwinn <ehron@local>
2018-12-21 10:30:20 -0500

    but use the right name this time

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,6 @@ def read_config():
 
 if __name__ == '__main__':
   conf = read_config()
-  db_client = db.DbClient(uri=conf['DATABASE']['Address'], prt=conf['DATABASE']['Port'], uname=conf['DATABASE']['User'], pw=conf['DATABASE']['Password'], db=conf['DATABASE']['Database'])
+  db_client = db_client.DbClient(uri=conf['DATABASE']['Address'], prt=conf['DATABASE']['Port'], uname=conf['DATABASE']['User'], pw=conf['DATABASE']['Password'], db=conf['DATABASE']['Database'])
 
   app.run(host='0.0.0.0', port=5000)
