995d1d301de85beef487d1e0953c95bebb34045b
Ehron Gwinn <ehron@local>
2018-12-24 22:06:28 -0500

    routes added

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, request
+from datetime import datetime
 import configparser
+import json
+
+
 import db_client
 
 DATABASE = 'DATABASE'
+dbc = None
 
 app = Flask(__name__)
 
@@ -12,8 +17,25 @@ def read_config():
     conf.read_file(f)
   return conf
 
+@app.route('/customers', methods=['GET'])
+def get_customers():
+    global dbc
+    customers = dbc.get_customer_records(10)
+    print(customers)
+    return json.dumps(customers)
+
+@app.route('/customers', methods=['POST'])
+def create_customer():
+    global dbc
+    print(dict(request.form))
+    customer = {k:v[0] for (k,v) in dict(request.form).items()}
+    print(customer)
+    new_record = dbc.insert_customer_record(customer)
+    print(new_record)
+    return json.dumps(new_record)
+
 if __name__ == '__main__':
   conf = read_config()
-  db_client = db_client.DbClient(uri=conf['DATABASE']['Address'], prt=conf['DATABASE']['Port'], uname=conf['DATABASE']['User'], pw=conf['DATABASE']['Password'], db=conf['DATABASE']['Database'])
+  dbc = db_client.DbClient(uri=conf['DATABASE']['Address'], prt=conf['DATABASE']['Port'], uname=conf['DATABASE']['User'], pw=conf['DATABASE']['Password'], db=conf['DATABASE']['Database'])
 
   app.run(host='0.0.0.0', port=5000)
--- backend/db_client.py
+++ backend/db_client.py
@@ -1,13 +1,14 @@
 import mysql.connector
 from mysql.connector import errorcode
+import datetime
 
 customer_table = '''
 CREATE TABLE IF NOT EXISTS `customers` (
     `cust_no` int(11) NOT NULL AUTO_INCREMENT,
-    `birth_date` date NOT NULL,
+    `birth_date` varchar(255) NOT NULL,
     `first_name` varchar(32) NOT NULL,
     `last_name` varchar(32) NOT NULL,
-    `create_date` date NOT NULL,
+    `create_date` varchar(255) NOT NULL,
     `social_security_number` varchar(16) NOT NULL,
     `address` varchar(255) NOT NULL,
     `salary` varchar(16) NOT NULL,
@@ -16,11 +17,19 @@ CREATE TABLE IF NOT EXISTS `customers` (
 
 class DbClient:
     conn = None
+    cursor = None
+    
+
 
     def __init__(self, uri, prt, uname, pw, db):
         self.init_db(uri, prt, uname, pw, db)
 
     def init_db(self, uri, prt, uname, pw, db):
+        self.uri = uri
+        self.port = prt
+        self.username = uname
+        self.password = pw
+        self.db = db
         self.connect_db(uri, prt, uname, pw)
         cursor = self.conn.cursor()
         print("Preparing database {}...".format(db))
@@ -28,7 +37,18 @@ class DbClient:
         cursor.execute('USE `{}`'.format(db))
         print("Preparing customer table...")
         cursor.execute(customer_table)
+        self.conn.commit()
         cursor.close()
+
+    def _execute_sql(self,sql,cursor):
+        try:
+            cursor.execute(sql)
+            return 1
+        except mysql.connector.errors.OperationalError as e:            
+            if e[0] == 2006:
+                print('Error encountered: {}.  Reconnecting db...'.format(e))
+                self.init_db(self.uri, self.port, self.username, self.password, self.db)
+                return 0
 
     def connect_db(self, uri, prt, uname, pw):
         print('Connecting to {} with username {} and password {}'.format(uri, uname, pw))
@@ -42,10 +62,38 @@ class DbClient:
             else:
                 print(err)
 
-    def get_customer_records(self, num):
+    def get_customer_records(self, num = None):
+        if num is None:
+            num = 10
+        statement = 'SELECT * FROM `customers` LIMIT {}'.format(num)
         cursor = self.conn.cursor()
+        self._execute_sql(statement, cursor)
+        results = []
+        for row in cursor:
+            r = {}
+            r['customer_number'] = row[0]
+            r['birth_date'] = row[1]
+            r['first_name'] = row[2]
+            r['last_name'] = row[3]
+            r['create_date'] = row[4]
+            r['ssn'] = row[5]
+            r['address'] = row[6]
+            r['salary'] = row[7]
+            results.append(r)
+        return results
 
-    # Records should be a dictionary of one or more customer records like:
-    # 
-    def insert_customer_records(self, records):
+    def insert_customer_record(self, record):
+        statement = '''INSERT INTO `customers` (`birth_date`, `first_name`, `last_name`, `create_date`, `social_security_number`, `address`, `salary`) 
+                        VALUES  ("{}", "{}", "{}", "{}", "{}", "{}", "{}");'''.format(record['birth_date'], record['first_name'], record['last_name'], record['create_date'], record['ssn'], record['address'], record['salary'] )
         cursor = self.conn.cursor()
+        self._execute_sql(statement, cursor)
+        self.conn.commit()
+        return self.get_customer_records()
+
+    def update_customer_record(self, record):
+        statement = '''INSERT INTO `customers` (`birth_date`, `first_name`, `last_name`, `create_date`, `social_security_number`, `address`, `salary`) 
+                        VALUES  ("{}", "{}", "{}", "{}", "{}", "{}", "{}");'''.format(record['birth_date'], record['first_name'], record['last_name'], record['create_date'], record['ssn'], record['address'], record['salary'] )
+        print(statement)
+        cursor = self.conn.cursor()
+        self._execute_sql(statement, cursor)
+        return self.get_customer_records()
